6d4d8b584e065cf5e87e8d0364cef4cf3ed77393
andrii_h <galgayand@gmail.com>
2023-03-21 15:55:11 +0200

    update btn

--- src/components/MyButton/MyButton.jsx
+++ src/components/MyButton/MyButton.jsx
@@ -1,15 +1,11 @@
 import s from './MyButton.module.css';
 import { NavLink } from 'react-router-dom';
 
-export const MyButton = ({ navigateTo, textBtn, type }) => {
+export const MyButton = ({ navigateTo, textBtn, active }) => {
+  console.log(active);
+
   return (
-    <NavLink
-      className={s.btn}
-      onClick={() => {
-        // console.log(navigateTo);
-      }}
-      to={navigateTo}
-    >
+    <NavLink className={`${s.btn} ${active ? s.active : ''}`} to={navigateTo}>
       <div className={s.textBtm}>{textBtn}</div>
     </NavLink>
   );
